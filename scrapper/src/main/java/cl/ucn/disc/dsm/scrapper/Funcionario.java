4c71b7b621c4a08e89f2a156eecbc93abee4d9a4
Nicolas Barrios <nicolas.barrios@alumnos.ucn.cl>
2021-10-31 04:03:39 -0300

    fix: fixed scraping process

--- scrapper/src/main/java/cl/ucn/disc/dsm/scrapper/Funcionario.java
+++ scrapper/src/main/java/cl/ucn/disc/dsm/scrapper/Funcionario.java
@@ -1,4 +1,64 @@
 package cl.ucn.disc.dsm.scrapper;
 
-public class Funcionario {
+
+import lombok.Builder;
+import lombok.Getter;
+
+
+/**
+ * The UCN Funcionario.
+ *
+ * @author Nicolas Barrios-Avendaño
+ */
+@Builder
+public final class Funcionario {
+
+    /**
+     * The ID.
+     */
+    @Getter
+    private Integer id;
+
+    /**
+     * The Nombre.
+     */
+    @Getter
+    private final String nombre;
+
+    /**
+     * The Cargo.
+     */
+    @Getter
+    private final String cargo;
+
+    /**
+     * The Unidad.
+     */
+    @Getter
+    private final String unidad;
+
+    /**
+     * The Email.
+     */
+    @Getter
+    private final String email;
+
+    /**
+     * The Telefono.
+     */
+    @Getter
+    private final String telefono;
+
+    /**
+     * The Oficina.
+     */
+    @Getter
+    private final String oficina;
+
+    /**
+     * The Direccion.
+     */
+    @Getter
+    private final String direccion;
+
 }
--- scrapper/src/main/java/cl/ucn/disc/dsm/scrapper/TheMain.java
+++ scrapper/src/main/java/cl/ucn/disc/dsm/scrapper/TheMain.java
@@ -2,15 +2,21 @@ package cl.ucn.disc.dsm.scrapper;
 
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-
+import java.io.File;
 import java.io.IOException;
-
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import lombok.extern.slf4j.Slf4j;
+
 
 /**
  * Main class to scrappe the Directorio Telefonico of UCN.
@@ -19,6 +25,10 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class TheMain {
+
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
     /**
      * The starting point
      * @param args to use
@@ -28,6 +38,9 @@ public class TheMain {
 
 
         log.debug("Starting the scrapping ..");
+
+        //The id
+        int id = 142;
 
         //connect and get the Document
         Document doc = Jsoup.connect("https://admision01.ucn.cl/directoriotelefonicoemail/fichaGenerica/?cod=142").get();
@@ -41,8 +54,27 @@ public class TheMain {
         String oficina = doc.getElementById("lblOficina").text();
         String direccion = doc.getElementById("lblDireccion").text();
 
+        //Call The constructor
+        Funcionario f = Funcionario.builder().id(id)
+                .nombre(nombre)
+                .cargo(cargo)
+                .unidad(unidad)
+                .email(email)
+                .telefono(telefono)
+                .oficina(oficina)
+                .direccion(direccion)
+                .build();
 
-        log.debug("Nombre: {}, Cargo: {}", nombre, cargo);
+        //The list of Funcionarios
+        List<Funcionario> funcionarios = new ArrayList<>();
+
+        // Add the Funcionario into the list
+        funcionarios.add(f);
+
+        // Write the list of Funcionarios in JSON format
+        FileUtils.writeStringToFile(new File("funcionarios.json"),
+                GSON.toJson(funcionarios),
+                StandardCharsets.UTF_8);
 
         log.debug("Done.");
     }
